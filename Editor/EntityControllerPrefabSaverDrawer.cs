60b215ec61fe83f7ab3f23907d630e194f8a700d
Paul Sztajer <paul@sztajer.org>
2018-05-28 17:02:41 +0200

    fixing PrefabSaver functionality

--- Editor/EntityControllerPrefabSaverDrawer.cs
+++ Editor/EntityControllerPrefabSaverDrawer.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
+using Component = UnityEngine.Component;
+using EditorCoroutines;
 
 namespace PDYXS.ThingSpawner
 {
@@ -15,39 +20,46 @@ namespace PDYXS.ThingSpawner
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            var target = property.serializedObject.targetObject;
+            var go = (target as Component).gameObject;
+            if (PrefabUtility.FindPrefabRoot(go) != go)
+            {
+                return;
+            }
+            
             EditorDrawer.Button(position, "Save Prefab", new Color(0.5f, 0.5f, 1),
             () =>
             {
                 var mono = property.serializedObject.targetObject as MonoBehaviour;
-                Dictionary<GameObject, Transform> parentStore = new Dictionary<GameObject, Transform>();
-                foreach (var c in mono.GetComponentsInChildren<MonoBehaviour>()) {
-                    if (c is IPrefabSaveable)
+
+                var toSave = mono.GetComponentsInChildren<IPrefabSaveable>(true).ToList().ConvertAll(
+                        (a) => (a as Component).gameObject
+                    );
+                toSave.Add(mono.gameObject);
+
+                toSave.RemoveAll((g) => PrefabUtility.FindPrefabRoot(g) != g);
+
+                var oldHideFlags = new Dictionary<GameObject, HideFlags>();
+                foreach (var go1 in toSave)
+                {
+                    oldHideFlags[go1] = go1.hideFlags;
+                    go1.hideFlags = HideFlags.DontSave;
+                }
+
+                foreach (var go2 in toSave)
+                {
+                    go2.hideFlags = HideFlags.None;
+                    if (PrefabUtility.GetPrefabType(go2) == PrefabType.DisconnectedPrefabInstance)
                     {
-                        parentStore[c.gameObject] = c.transform.parent;
+                        PrefabUtility.ReconnectToLastPrefab(go2);
                     }
+                    PrefabUtility.ReplacePrefab(go2, PrefabUtility.GetPrefabParent(go2), ReplacePrefabOptions.ConnectToPrefab);
+                    go2.hideFlags = HideFlags.DontSave;
                 }
 
-                foreach (var stuff in parentStore) {
-                    stuff.Key.transform.SetParent(mono.transform.parent);
-                    stuff.Key.transform.localScale = Vector3.one;
-                }
-
-                foreach (var stuff in parentStore) {
-                    var hf = stuff.Key.gameObject.hideFlags;
-                    stuff.Key.gameObject.hideFlags = HideFlags.None;
-                    if (PrefabUtility.GetPrefabType(stuff.Key) == PrefabType.DisconnectedPrefabInstance)
-                    {
-                        PrefabUtility.ReconnectToLastPrefab(stuff.Key);
-                    }
-                    PrefabUtility.ReplacePrefab(stuff.Key, PrefabUtility.GetPrefabParent(stuff.Key));
-                    PrefabUtility.RevertPrefabInstance(stuff.Key);
-                    stuff.Key.gameObject.hideFlags = hf;
-                }
-
-                foreach (var stuff in parentStore) {
-                    stuff.Key.transform.SetParent(stuff.Value, false);
-                    stuff.Key.transform.localScale = Vector3.one;
-                    EditorUtility.SetDirty(stuff.Key);
+                foreach (var go3 in toSave)
+                {
+                    go3.hideFlags = oldHideFlags[go3];
                 }
             });
         }
